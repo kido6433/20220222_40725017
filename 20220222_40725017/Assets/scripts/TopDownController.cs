20e1b509cf9ffbd7ab24280245b49cc2afcd5f63
kido6433 <100185402+kido6433@users.noreply.github.com>
2022-03-22 16:03:22 +0800

    角色控制

--- 20220222_40725017/Assets/scripts/TopDownController.cs
+++ 20220222_40725017/Assets/scripts/TopDownController.cs
@@ -6,11 +6,14 @@ namespace kido
     {
         #region 資料
         #endregion
+        [SerializeField,Header("移動速度"),Range(0,100)]
         private float speed = 10.5f;
         private string parameterRun = "開關跑步";
         private string parameterDead = "開關死亡";
         private Animator ani;
         private Rigidbody2D rig;
+        private float h;
+        private float v;
 
         #region 事件
         private void Awake()
@@ -24,12 +27,30 @@ namespace kido
 
         private void Update()
         {
-            print("我在 Update 事件內");
+            GetInput();
+            Move();
         }
         #endregion
 
         #region 方法
         #endregion
+
+        private void GetInput()
+        {
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+            //print("取得水平軸像值：" + h);
+
+        }
+
+        private void Move()
+        {
+            rig.velocity = new Vector2(h, v) * speed;
+
+            ani.SetBool(parameterRun, h != 0 || v != 0  );
+
+            transform.eulerAngles = new Vector3(0, h >= 0 ? 0 : 180, 0) ;
+        }
     }
 }
 
